9750467bce5492d7078e14d8856aab1c8deba1c2
cdo27 <cdoo@sfu.ca>
2023-11-17 12:35:51 -0800

    added all php

---
+++ modeldetails.php
@@ -0,0 +1,42 @@
+<?php
+    if(isset($_SERVER['HTTPS']) &&  $_SERVER['HTTPS']== "on") {
+		header("Location: http://" . $_SERVER['HTTP_HOST'] .
+			$_SERVER['REQUEST_URI']);
+		exit();
+	}
+
+    $servername = "localhost";
+    $username = "root"; //login with root
+    $password = "";
+    $dbname = "classicmodels"; //classicmodels.sql
+    
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    //check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $prodCode = $_GET['productCode'];
+    $query = "SELECT * FROM products WHERE productCode = ?"; 
+    $result = $conn->prepare($query);
+    $result->bind_param('s', $prodCode);
+    $result->execute();
+    $result->bind_result($prCode,$prName,$prLine,$prScale,$prVendor,$prDesc,$prQ,$prPrice,$MSRP);
+
+    if($result->fetch()) {
+        echo "<h3>$prName</h3>\n";
+        echo "<p>Category: $prLine, Scale: $prScale, Vendor: $prVendor, Price: \$$prPrice</p>\n";
+        echo "<p>Description: $prDesc</p>\n";
+        }
+    $result->free_result();
+
+    if(isset($_SESSION['valid_user'])){ //if user is logged in and this model is not in watchlsit
+
+    }
+
+
+
+    $conn->close();
+?>
---
+++ showmodels.php
@@ -0,0 +1,52 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Classic Models</title>
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <ul class="menu-bar">
+            <li class="menu-item"><a href="showmodels.php">Models</a></li>
+            <li class="menu-item"><a href="addtowatchlist.php">Watchlist</a></li>
+            <li class="menu-item"><a href="login.php">Login</a></li>
+        </ul>
+
+    </body>
+</html>
+
+<?php
+    if(isset($_SERVER['HTTPS']) &&  $_SERVER['HTTPS']== "on") {
+		header("Location: http://" . $_SERVER['HTTP_HOST'] .
+			$_SERVER['REQUEST_URI']);
+		exit();
+	}
+
+    $servername = "localhost";
+    $username = "root"; //login with root
+    $password = "";
+    $dbname = "classicmodels"; //classicmodels.sql
+    
+    //create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    
+    //check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $query = "SELECT productCode, productName FROM products";
+    $result = $conn->query($query);
+
+    echo "<h2>All Models</h2>";
+
+    echo "<ul class = 'modellist'>";
+    while ($row = $result->fetch_row()) {
+        echo "<li>";
+        echo "<a href=\"modeldetails.php?productCode=$row[0]\">$row[1]</a>";
+        echo "</li>\n";
+    };
+    echo "</ul>";
+
+    $conn->close();
+?>
