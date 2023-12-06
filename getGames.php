aa05f2facf08c9ddc38f08bdccfacb2dfc5e721d
Winnie <wsa48@sfu.ca>
2023-12-06 14:53:09 -0800

    Auto stash before merge of "main" and "origin/main"

--- getGames.php
+++ getGames.php
@@ -1,55 +1,65 @@
+<?php
+// Database connection code here
+
+// Fetch products based on selected category
+if (isset($_POST['category'])) {
+    $category = $_POST['category'];
+
+    $query = "SELECT * FROM products";
+
+    if (!empty($category)) {
+        $query .= " WHERE category = '$category'";
+    }
+
+    // Execute the query and fetch products
+    // Implement your database connection and query execution code here
+    // Example:
+    // $result = mysqli_query($connection, $query);
+
+    // Display the filtered products
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div>{$row['name']}</div>";
+    }
+}
+?>
+
+
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<style>
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-
-table, td, th {
-  border: 1px solid black;
-  padding: 5px;
-}
-
-th {text-align: left;}
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax Filtering</title>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 </head>
 <body>
 
-<?php
-$q = intval($_GET['q']);
+<label for="category">Select Category:</label>
+<select id="category" name="category" onchange="filterProducts()">
+    <option value="">All Categories</option>
+    <option value="Electronics">Electronics</option>
+    <option value="Clothing">Clothing</option>
+    <!-- Add more categories as needed -->
+</select>
 
-$servername = "localhost";
-$username = "root"; //login with root
-$password = "";
-$dbname = "gamearchive"; //classicmodels.sql
+<div id="products-container">
+    <!-- Product list will be displayed here -->
+</div>
 
-//create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+<script>
+function filterProducts() {
+    var category = $("#category").val();
 
-//check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $.ajax({
+        type: "POST",
+        url: "filter.php",
+        data: { category: category },
+        success: function(response) {
+            $("#products-container").html(response);
+        }
+    });
 }
+</script>
 
-mysqli_select_db($con,"gamearchive");
-$sql="SELECT Title  FROM video_games_2022 WHERE Genre = '".$q."'";
-$result = mysqli_query($con,$sql);
-
-echo "<table>
-<tr>
-<th>Game title</th>
-
-</tr>";
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr>";
-  echo "<td>" . $row['Title'] . "</td>";
-  //echo "<td>" . $row['Genre'] . "</td>";
-  echo "</tr>";
-}
-echo "</table>";
-mysqli_close($con);
-?>
 </body>
 </html>
